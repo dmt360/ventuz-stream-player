53ee7d7ca5bb5684fb2136ff1b7b0b0305b6f914
Tammo Hinrichs <tammo.hinrichs@ventuz.com>
2025-03-27 15:05:27 +0100

    Better bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import type { UserConfig } from 'vite'
+import type { UserConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default {
-
     server: {
-        port: 22444
-    }
-    
-} satisfies UserConfig
+        port: 22444,        
+    },
+
+    plugins: [cssInjectedByJsPlugin()],
+
+    build: {
+        assetsDir: '.',
+        outDir: 'dist',
+
+        rollupOptions: {
+            output: {
+                entryFileNames: 'ventuz-stream-player-min.js'
+            }
+        },
+    },
+
+} satisfies UserConfig;
